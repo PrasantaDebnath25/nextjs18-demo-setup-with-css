dc23f60cd22c3767e984edf434543203a2218287
PrasantaDebnath25 <prasantadebnath.2013@gmail.com>
2023-09-16 01:51:05 +0530

    App file scss written

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,7 @@
 import '@/styles/globals.css'
 // import '@/scss'
+// import "../scss/main.scss"; //Earlier
+import "@/scss/main.scss"; // Now
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
